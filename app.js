3bed60d646c080e54169bb56da405c7ea9bd0a09
Sardor081902 <erkinovsardor72>
2025-11-23 18:33:13 +0500

    patch 0.0.0.7

--- app.js
+++ app.js
@@ -54,23 +54,70 @@ navshow.addEventListener('click', () => {
   }
 })
 
+//Dark mode Section
+
 const black__mode = document.querySelector('.bt')
 const main = document.querySelector('main')
-const about__text = document.querySelector('.about__text')
+const about__title = document.querySelector('.about__title')
 const sub__title = document.querySelector('.sub__title')
+const about__text = document.querySelector('.about__text')
+const icons = document.querySelectorAll('.icons')
+const web = document.querySelector('.web')
+const my__name = document.querySelector('.my__name')
+const nav = document.querySelector('#homebox')
+const logo = document.querySelector('.logo')
+const sardor = document.querySelector('.sardor')
+const my__photo = document.querySelector('.my__photo')
+const skills__text = document.querySelector('.skills__text')
+const project__text = document.querySelector('.project__text')
+const header__active = document.querySelector('.header.active')
 
 black__mode.addEventListener('click', () => {
   black__mode.classList.toggle('active');
 
   if
     (black__mode.classList.contains('active')) {
-    homeBox.style.backgroundColor = 'black';
-    main.style.backgroundColor = 'black';
-    about__text.style.color = 'blue';
-    
+    homeBox.style.backgroundColor = '#121212';
+    main.style.backgroundColor = '#121212';
+    about__title.style.color = 'white';
+    about__text.style.color = 'white';
+    about__text.style.backgroundColor = '#121212';
+    about__text.style.boxShadow = 'INSET 0px 0px 10px 3px rgba(255, 0, 0, 0.12)';
+    icons.forEach(icon => {
+      icon.style.color = 'red';
+    });  
+    web.style.color = 'red';
+    my__name.style.color = 'white';
+    sub__title.style.color = 'white';
+    nav.style.backgroundColor = '#121212';
+    nav.style.boxShadow = '0px 0px 10px 3px rgba(255, 0, 0, 0.12)';
+    logo.style.color = 'white';
+    sardor.style.color = 'red';
+    my__photo.style.boxShadow = '0px 0px 50px 3px rgba(255, 0, 0, 0.12)';
+    skills__text.style.color = 'white';
+    project__text.style.color = 'white';
+    header__active.style.color = 'red';
+
   } else {
     homeBox.style.backgroundColor = '';
     main.style.backgroundColor = '';
+    about__title.style.color = '';
     about__text.style.color = '';
+    about__text.style.backgroundColor = '';
+    about__text.style.boxShadow = '';
+    icons.forEach(icon => {
+      icon.style.color = '';
+    });
+    web.style.color = '';
+     my__name.style.color = '';
+     sub__title.style.color = '';
+    logo.style.color = '';
+    sardor.style.color = '';
+    my__photo.style.boxShadow = '';
+    nav.style.backgroundColor = '';
+    nav.style.boxShadow = '';
+      skills__text.style.color = '';
+      project__text.style.color = '';
+      header__active.style.color = '';
   }
 })
